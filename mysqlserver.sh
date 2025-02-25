e95d9d7a90813ced2bf2b07cba3b4d52889fd553
Murali Elavarthi <muralis2p@gmail.com>
2025-02-25 09:07:24 +0530

    final-commit3

--- mysqlserver.sh
+++ mysqlserver.sh
@@ -56,7 +56,7 @@ validate $? "mysql-server restarted"
 systemctl enable mysqld &>>$LOG_FILE 
 validate $? "mysql-server enabled"
 
-mysql -h 172.31.82.58 -u root -pExpenseApp@1 -e 'show databases;'
+mysql -h 172.31.82.58 -u root -pExpenseApp@1 -e 'show databases;'&>>$LOG_FILE 
 
 if [ $? -ne 0 ]
 then
