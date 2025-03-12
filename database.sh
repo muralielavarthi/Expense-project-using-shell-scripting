4f4c362bbbeb2a92da57e4b917e83497ad8e89d3
Murali Elavarthi <muralis2p@gmail.com>
2025-03-13 04:57:06 +0530

    c1

--- database.sh
+++ database.sh
@@ -13,9 +13,10 @@ fi
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 installation Success"
+        echo "$2 Success"
     else
-        echo "$2 installation Failure"
+        echo "$2 Failure"
+        exit 1
     fi
 }
 echo "script started executing at $TIME_STAMP"
@@ -28,8 +29,22 @@ then
 else
     echo "installing mysql-server"
     dnf install mysql-server -y
-    VALIDATE $? "mysql-server"
+    VALIDATE $? "mysql-server installation"
 fi
 
+systemctl start mysqld
+VALIDATE $? "start mysqld"
+
+systemctl enable mysqld
+VALIDATE $? "enable mysqld"
+
+mysql -h 172.31.24.29 -u root -pm@123 -e "show databases"
+if [ $? -eq 0 ]
+then
+    echo "root user passwd setup already done skipping..."
+else
+    mysql_secure_installation --set -root -pass m@123
+    VALIDATE $? "root user passwd setup"
+fi
 
 
